70908bb48c79cdd11050d43afb7134625ab66dd6
Maarten in 't Hout <maarten@twiks.nl>
2013-07-09 14:06:53 +0200

    upgrade to 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='MultiBus',
-    version='0.1.5',
+    version='0.1.6',
     packages=['multibus'],
     url='https://github.com/Hyperion-project/multibus',
     license='lgpl v3',
